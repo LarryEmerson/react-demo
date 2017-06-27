042a89b746fefddb8e57b69875d5c8d40b6392a9
LarryEmerson <LarryEmerson@e-MacBook-Pro.local>
2017-06-27 15:55:20 +0800

    repo详情页面添加readme.md的markdown显示

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.less';
-import {Menu, Icon, Switch, Tabs, Button, AutoComplete} from 'antd';
+import {Menu, Dropdown, Icon, Switch, Tabs, Button, AutoComplete, message} from 'antd';
 const TabPane = Tabs.TabPane;
 const ButtonGroup = Button.Group;
 const GithubHost = 'https://github.com'
@@ -8,45 +8,45 @@ const GithubApiHost = 'https://api.github.com'
 const client_id = 'client_id=1b847c10864b9d86fe24'
 const client_secret = 'client_secret=ee2975df30bbe5bc1bfa169299fd12cac881065b'
 const appOAuth = '?' + client_id + '&' + client_secret
-
-
+var base64 = require('base-64');
+var utf8 = require('utf8');
+var ReactMarkdown = require('react-markdown');
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            githubUser: undefined,
-            userInfo: undefined,
-            repos: [],
-            enableForks: false,
-            panes: [],
-            activeKey: undefined,
-            contributors: [],
-            searchHistory: [],
-
-            loginStatus: 0,
-            loginModal: false,
-            loginLoading: false,
-
-            requestOauth: false,
-
-            access_token: undefined,
+            userInfo: undefined,//current user's info
+            loggedUserInfo: undefined,//current user's info
+            showUserInfoMenu: false,
+            repos: [],//repos of current user
+            enableForks: false,//show or hide current user's forked repos
+            panes: [],//opened panes & dataSource of Tabs
+            activeKey: undefined,//current active pane
+            contributors: [],//contributors of current selected repo
+            searchHistory: [],//successfully serched github usernames
+            readme: "",
+            access_token: undefined,//current access_token for api calling
+            loginLoading: undefined,//is login status
         }
+        message.config({
+            duration: 1.5
+        })
     }
 
 
-    goToGithub() {
+    goToGithub() {//go to current user's github website
         if (this.state.userInfo) {
             this.goToGithubUser(this.state.userInfo)
         }
     }
 
-    goToGithubUser(user) {
+    goToGithubUser(user) {//go to specific user's github website
         if (user && user.html_url) {
             window.open(user.html_url);
         }
     }
 
-    getUserInfo(user) {
+    getUserInfo(user, login) {//get userinfo for specific user, also repos if the user exists
         // console.log('getUserInfo', user)
         if (user) {
             let context = this;
@@ -61,10 +61,16 @@ class App extends Component {
                         })) {
                         searchH.push(user)
                     }
+                    if (login) {
+                        context.state.loggedUserInfo = json
+                    }
                     context.setState({
                         searchHistory: searchH,
-                        userInfo: json, githubUser: user, repos: [],
-                        panes: [], activeKey: undefined, contributors: []
+                        userInfo: json,
+                        repos: [],
+                        panes: [],
+                        activeKey: undefined,
+                        contributors: []
                     })
                     context.getUserRepos(json.repos_url)
                 })
@@ -74,7 +80,7 @@ class App extends Component {
         }
     }
 
-    getUserRepos(url) {
+    getUserRepos(url) {//get repos of user with url
         let context = this;
         fetch(url + appOAuth, {
             method: 'GET'
@@ -83,15 +89,13 @@ class App extends Component {
                 // console.log(json)
                 context.state.repos = json;
                 context.setState(context.state.repos)
-
-
             })
         }).catch(function (error) {
             console.log(error)
         });
     }
 
-    getContributors(url) {
+    getRepoContributors(url) {//get contributors with url
         let context = this;
         if (url) {
             fetch(url + appOAuth, {
@@ -105,15 +109,36 @@ class App extends Component {
                     context.setState({contributors: []})
                 }
             }).catch(function (error) {
-                console.log('getContributors error:', error)
+                console.log('getRepoContributors error:', error)
                 context.setState({contributors: []})
             });
         } else {
             context.setState({contributors: []})
         }
+        // context.getRepoReadme("https://api.github.com/repos/git/git/contents/readme.md")
     }
 
-    handleRepoClick(item, key, keyPath) {
+    getRepoReadme(url) {//get readme with url
+        let context = this;
+        fetch(url.replace("{+path}", "README.md"), {
+            method: "GET"
+        }).then(function (res) {
+            if (res.status == 200) {
+                res.json().then(function (json) {
+                    var bytes = base64.decode(json.content);
+                    var text = utf8.decode(bytes);
+                    // console.log(text);
+                    context.setState({readme: text})
+                })
+            } else {
+                context.setState({readme: ""})
+            }
+        }).catch(function (error) {
+            context.setState({readme: ""})
+        })
+    }
+
+    handleRepoClick(item, key, keyPath) {//handle repo clicked from repolist
         const panes = this.state.panes;
         const activeKey = item.key;
         var repo = this.state.repos.filter(function (element, index, array) {
@@ -131,38 +156,11 @@ class App extends Component {
             panes.push({title: repo.name, content: repo.description, key: activeKey});
             this.setState({panes, activeKey});
         }
-        this.getContributors(repo.contributors_url)
+        this.getRepoContributors(repo.contributors_url)
+        this.getRepoReadme(repo.contents_url)
     }
 
-    handlePaneEdit = (targetKey, action) => {
-        this[action](targetKey);
-    }
-    remove = (targetKey) => {
-        let activeKey = this.state.activeKey;
-        let lastIndex;
-        this.state.panes.forEach((pane, i) => {
-            if (pane.key == targetKey) {
-                lastIndex = i - 1;
-            }
-        });
-        const panes = this.state.panes.filter(pane => pane.key !== targetKey);
-        if (lastIndex >= 0 && activeKey == targetKey) {
-            activeKey = panes[lastIndex].key;
-        }
-        this.setState({panes, activeKey});
-    }
-    handleActivePane = (activeKey) => {
-        this.setState({activeKey});
-        var repo = this.state.repos.filter(function (element, index, array) {
-            return element.id == activeKey;
-        });
-        if (repo.length > 0) {
-            repo = repo[0]
-            this.getContributors(repo.contributors_url)
-        }
-    }
-
-    reversalColor(a) {
+    reversalColor(a) {//get reversal color with color
         a = a.replace('#', '');
         var c16, c10, max16 = 15, b = [];
         for (var i = 0; i < a.length; i++) {
@@ -173,18 +171,15 @@ class App extends Component {
         return '#' + b.join('');
     }
 
-    handleLogin() {
-        if (this.state.loginStatus) {
-            this.getUserInfo(this.state.userInfo.login)
+    handleLogin() {//handle login btn
+        if (this.state.access_token && this.state.loggedUserInfo) {
+            this.setState({showUserInfoMenu: true})
         } else {
-            this.handleModalLogin();
-            // this.setState({loginModal: true})
+            this.handleLoginRequest();
         }
     }
 
-
-
-    componentDidMount() {
+    componentDidMount() {//get access_token with code from href, auto oauth when access is invalid
         let context = this;
         // console.log('componentDidMount', this.state.code, window.location.href)
         var url = window.location.href;	  //获取当前页面的url
@@ -216,45 +211,99 @@ class App extends Component {
                                 if (res.status == 200) {
                                     res.json().then(function (json) {
                                         // console.log('user', json)
-                                        context.getUserInfo(json.login)
+                                        context.getUserInfo(json.login, true)
+                                        message.destroy()
                                     })
+                                } else {
+                                    context.handleLoginRequest()
                                 }
-                            })
-
+                            }).catch(function (error) {
+                                context.handleLoginRequest()
+                                message.destroy()
+                            });
+                        } else {
+                            message.destroy()
                         }
                     })
+                } else {
+                    message.destroy()
                 }
+            }).catch(function (error) {
+                console.log('github_oauth_access_token error:', error)
+                message.destroy()
             });
         } else {
             context.getUserInfo('git')
         }
     }
 
-    handleModalLogin() {
+    handleLoginRequest() {//
         let context = this;
+        if (context.state.loginLoading) {
+            return
+        }
+        context.setState({loginLoading: message.loading('Login in progress..', 0)})
         var dataStr = (new Date()).valueOf();
-        context.setState({requestOauth: true})
         let url = GithubHost + "/login/oauth/authorize?" + client_id + "&scope=user&state=" + dataStr
         // console.log(url)
         window.location.href = url
     }
 
+    handleLogOut() {
+        this.setState({
+            loggedUserInfo: {},
+            access_token: undefined,
+            showUserInfoMenu: false,
+        })
+    }
+
+    handleMenuClick = (e) => {
+        if (e.key === '1') {//1 for logged user
+            this.getUserInfo(this.state.loggedUserInfo.login)
+        } else if (e.key === '2') {//2 for log out
+            this.handleLogOut()
+        }
+    }
+
     render() {
         let context = this;
-        let menuSnippet = () => {
-            return context.state.repos.filter(function (repo) {
-                return context.state.enableForks ? true : repo.fork == false;
-            }).map(function (repo) {
-                return <Menu.Item key={repo.id} className="gitItem">
-                    <Icon type="github"/>
-                    <span
-                        style={{overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap',}}>
-                        {repo.name}
-                    </span>
-                </Menu.Item>
-            })
+        let loginFloatBtn = () => {//the floating login btn locating at the top right corner
+            return (
+                <Dropdown
+                    trigger={['click']}
+                    placement="bottomRight"
+                    overlay={
+                        context.state.showUserInfoMenu ?
+                            <Menu onClick={context.handleMenuClick}>
+                                <Menu.Item key="1">My Github</Menu.Item>
+                                <Menu.Divider />
+                                <Menu.Item key="2">Log out</Menu.Item>
+                            </Menu>
+                            :
+                            <Menu/>
+                    }>
+                    <Button
+                        className="login"
+                        onClick={context.handleLogin.bind(context)}
+                    >
+                        {(() => {
+                            return (
+                                context.state.access_token && context.state.loggedUserInfo
+                                    ?
+                                    <img
+                                        src={ context.state.loggedUserInfo.avatar_url}
+                                        alt=""
+                                    />
+                                    :
+                                    <Icon type="github"/>
+                            )
+                        })()}
+                    </Button>
+                </Dropdown>
+
+            )
         }
-        let userInfo = () => {
+        let userInfo = () => {//top left userinfo
             return (
                 context.state.userInfo
                     ?
@@ -272,7 +321,21 @@ class App extends Component {
                     </div>
             )
         }
-        let contributorsSnippet = () => {
+        let repoList = () => {//repolist in left sidebar
+            return context.state.repos.filter(function (repo) {
+                return context.state.enableForks ? true : (!repo.fork);
+            }).map(function (repo) {
+                return <Menu.Item key={repo.id} className="gitItem">
+                    <Icon type="github"/>
+                    <span
+                        style={{overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap',}}>
+                        {repo.name}
+                    </span>
+                </Menu.Item>
+            })
+        }
+
+        let contributorsSnippet = () => {//contributors of repo in a pane
             return (
                 context.state.contributors.map(function (item) {
                     let randomColor = '#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).slice(-6)
@@ -290,7 +353,8 @@ class App extends Component {
                 })
             )
         }
-        let paneSnippet = (key) => {
+
+        let paneSnippet = (key) => {//pane in tab
             let repo = context.state.repos.filter(function (element) {
                 return element.id == key
             })[0]
@@ -335,27 +399,15 @@ class App extends Component {
 
                         <div className="description">
                             {repo.description}
+                            <p></p>
+                            <ReactMarkdown source={context.state.readme ? context.state.readme : ""}/>
                         </div>
                     </div>
 
                 </div >
             )
         }
-        let loginFloatBtn = () => {
-            return (
-                <Button className="login"
-                        onClick={context.handleLogin.bind(context)}
-                >
-                    {(() => {
-                        return context.state.loginStatus ?
-                            <img
-                                src={ "https://avatars1.githubusercontent.com/u/18133?v=3"}
-                                alt=''/>
-                            : <Icon type="github"/>
-                    })()}
-                </Button>
-            )
-        }
+
         return (
             <div className="view">
                 <div className="viewWrap">
@@ -376,7 +428,7 @@ class App extends Component {
                                 }}
                                 onSearch={(value) => {
                                     context.setState({searchInput: value})
-                                    context.getUserInfo(value)
+                                    // context.getUserInfo(value)
                                 }}
                                 onChange={(value) => {
                                     context.setState({searchInput: value})
@@ -384,9 +436,11 @@ class App extends Component {
                             >
                             </AutoComplete>
                             <Button
-                                icon="search" onClick={(value) => {
-                                context.getUserInfo(context.state.searchInput)
-                            }}/>
+                                icon="search"
+                                onClick={(value) => {
+                                    context.getUserInfo(context.state.searchInput)
+                                }}
+                            />
                         </div>
                         <div className='siderbarSwitch'>
                             <span>Enable Forks</span>
@@ -398,7 +452,7 @@ class App extends Component {
                         </div>
                         <Menu mode="vertical" style={{backgroundColor: 'transparent'}}
                               onClick={context.handleRepoClick.bind(context)}>
-                            {menuSnippet()}
+                            {repoList()}
                         </Menu>
                     </div>
                 </div>
@@ -406,10 +460,26 @@ class App extends Component {
                     <Tabs
                         className='tabpane'
                         hideAdd
-                        onChange={context.handleActivePane}
+                        onChange={
+                            (activeKey) => {
+                                this.setState({activeKey});
+                                var repo = context.state.repos.filter(function (element, index, array) {
+                                    return element.id == activeKey;
+                                });
+                                if (repo.length > 0) {
+                                    repo = repo[0]
+                                    context.getRepoContributors(repo.contributors_url)
+                                    context.getRepoReadme(repo.contents_url)
+                                }
+                            }
+                        }
                         activeKey={context.state.activeKey}
                         type="editable-card"
-                        onEdit={this.handlePaneEdit}
+                        onEdit={
+                            (targetKey, action) => {
+                                this[action](targetKey);
+                            }
+                        }
                         animated={{inkBar: true, tabPane: true}}
                     >
                         {this.state.panes.map(pane =>
